8d023644d6c1d8107f65fa9e9e0ed5f58a363099
Anas <85869106+geekboi777@users.noreply.github.com>
2022-06-06 23:40:24 +0530

    Create Guess_outputs.cpp

---
+++ Guess_outputs.cpp
@@ -0,0 +1,107 @@
+Q1)
+  
+#include <iostream>
+using namespace std;
+int main(){
+    cout << code ;
+}
+
+ // output = error 
+
+Q2)
+  
+  #include <iostream>
+using namespace std;
+int main(){
+    cout << "career" << "labs";
+}
+
+// output = careerlabs
+
+Q3)
+  
+  #include <iostream>
+using namespace std;
+int main()
+{
+       char c = 74;
+       cout << c;
+       return 0;
+}
+
+// output = J
+
+Q4)
+  
+  #include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    char ch = 'a';
+    ch = ch + a;
+    cout << ch << endl;
+}
+
+
+// output = k
+
+
+// IMP 
+Q5)
+  
+  #include <iostream>
+using namespace std;   
+int main()
+{
+    double a = 6 / 4;
+    int b  = 6 / 4;
+    double c = a + b;
+    cout << c;
+}
+
+// output = 2
+// lesson : if division is in between two integer types then the answer is converted to int and then stored.
+
+Q6)
+  #include <iostream>
+using namespace std;  
+int main()
+{
+    double a = 55.5;
+    int b = 55;
+    a = a % 10;
+    b = b % 10;
+    cout << a << " "  << b;
+}
+
+// output = error . % can't be used with double or float.
+
+
+Q7)
+  
+  #include <iostream>
+using namespace std;
+int main() 
+{
+    int var1 = 5;
+    int var2 = 6;
+    cout << (var1 > var2);
+ }
+
+
+// output = 0 
+
+Q8)
+  
+        if((1+x) != (1-x)) {
+          cout<<"In if block";
+         }
+For what values of the x is an if-statement block not executed ?
+Ans) 0
+  
+ 
+  Q9)
+  
+  
+
